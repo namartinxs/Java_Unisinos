a62838dd14daff8eee1655a9682767b2663fbf48
nathaliamartins <nathaliafloresmartins4550@gmail.com>
2025-03-06 21:33:14 -0300

    Aula 2 e 3

---
+++ App.java/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int a = 4, b = 5;
+        int c;
+        c = a / b;
+        System.out.println("c = " + c);
+   
+        double d;
+        d = a / b;
+        System.out.println("d = " + d);
+   
+        double e;
+        e = (double) a / b; //Conversão deve ser explícita
+        System.out.println("e = " + e);
+   
+    
+    }
+}
+//[] significa opcional 
