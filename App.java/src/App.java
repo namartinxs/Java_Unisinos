3c4490bf541276e72420159163fa657c6268aac7
nathaliamartins <nathaliafloresmartins4550@gmail.com>
2025-03-06 22:07:38 -0300

    COD.FINAL

--- App.java/src/App.java
+++ App.java/src/App.java
@@ -1,20 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        int a = 4, b = 5;
-        int c;
-        c = a / b;
-        System.out.println("c = " + c);
-   
-        double d;
-        d = a / b;
-        System.out.println("d = " + d);
-   
-        double e;
-        e = (double) a / b; //Conversão deve ser explícita
-        System.out.println("e = " + e);
-   
-    
+       double fSalario = 10000;
+       double IR =0;
+       
+       if ((fSalario >=1000)&& (fSalario <2500))
+       {
+            IR = fSalario * 0.1;
+       }
+            
+        else
+        {
+            if (fSalario < 5000)
+            {
+                IR = fSalario * 0.2;
+            }
+            else
+            {
+                IR = fSalario * 0.25;
+            }
+        }
+            
+     System.out.println(IR);
     }
 }
-//[] significa opcional 
+
